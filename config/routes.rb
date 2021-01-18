bfd009e3bf18078f8ec28c02b9d1a06481c36038
AZCONA, MARCOS <marcosazcona2000@hotmail.com>
2021-01-18 17:08:27 -0300

    Arreglada rutas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  ### El member me permite agregar con un metodo http distinto un controlador en las rutas con su path
+  ### Ej: export_all_path, export_path, ambos por put
   resources :books, param: :id_book do
     member do
       put 'export'
@@ -15,14 +17,16 @@ Rails.application.routes.draw do
     end
   end    
 
-  put '/books/export_all', to: 'books#export_all'
   
   authenticated :user do
     ### Redireccion al listado de libros una vez logueado
     root 'books#index', as: :authenticated_root
   end
-
-  #unauthenticated :user do
-  #  root 'application', as: :unauthenticated_root 
-  #end
+  
+  devise_scope :user do
+    ### En caso de no estar logueado redirecciona al login
+    root to: 'devise/sessions#new'
+  end
+  
+  #put '/books/export_all', to: 'books#export_all'
 end
